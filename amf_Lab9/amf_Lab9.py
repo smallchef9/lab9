64a39a6cb7060e55fcedb269f864f22b5fbb83a2
AnnaTeresa257 <annateresapriore@gmail.com>
2024-04-10 10:52:30 -0400

    FINAL

--- amf_Lab9/amf_Lab9.py
+++ amf_Lab9/amf_Lab9.py
@@ -7,12 +7,22 @@ def encode(password):
         encoded_password.append(encChar)
     return encoded_password
 
+
+def decode(password):
+    decoded_password = []
+    for i in password:
+        decChar = int(i) - 3
+        decoded_password.append(decChar)
+    return decoded_password
+
+
 def menu():
     print("Menu\n"
           "-------------\n"
           "1. Encode\n"
           "2. Decode\n"
           "3. Quit\n")
+
 
 def main():
     while True:
@@ -22,9 +32,13 @@ def main():
             password = input("Please enter your password to encode: ")
             encoded_password = encode(password)
             print("Your password has been encoded and stored!\n")
+            print()
         if choice == 3:
             break
         if choice == 2:
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}")
+            print()
 
 
 if __name__ == "__main__":
